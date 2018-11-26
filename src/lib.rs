9d22e9965f0621d72b25ba5545903560ef2cfa95
jonathan gerber <jlgerber@gmail.com>
2018-11-26 13:47:45 -0800

    fixed path to be bak/name/name-date/name

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 #![feature(specialization)]
 
-#[macro_use]
-extern crate pyo3;
 extern crate bakbuster;
 extern crate chrono;
-use std::boxed::Box;
-use pyo3::prelude::*;
-use pyo3::exceptions;
+#[macro_use]extern crate pyo3;
+
 use bakbuster::prelude::*;
-use std::{io::BufReader,fs::File, path::PathBuf};
 use chrono::prelude::*;
+use pyo3::{ prelude::*, exceptions };
+use std::{io::BufReader,fs::File, path::PathBuf};
 
 
 #[pyfunction]
 /// retrieves the file given the full path to the swinstalled file and a
 /// string formatted by using datetime.ctime() method
 ///
-/// # example
+/// :param string file:
+///     The full path to the installed file
+/// :param string datetime:
+///     The datetime to lock off on, adopting the datetime.ctime() format
+/// (eg 'Sat Feb 113456 2018')
 ///
-/// get_file_on(/dd/facility/etc/packages.xml, Sat Dec 23 09:31:46 2017)
+/// get_file_on('/dd/facility/etc/packages.xml', 'Sat Dec 23 09:31:46 2017')
 fn get_file_on(file: &str, datetime: &str) -> PyResult<String> {
     let mut pb = PathBuf::from(file);
-
     // get filename
     // for some reason ok_or(...) does not work here. I have to use the more verbose
     // match...
@@ -38,6 +39,7 @@ fn get_file_on(file: &str, datetime: &str) -> PyResult<String> {
     pb.push("bak");
     // push filename as a directory
     pb.push(filename.as_str());
+
     // build the swinstall stack file name and push it on the pathbuf
     pb.push(format!("{}_swinstall_stack", filename));
     // grab the directory from the path. Ideally we would do this before the
@@ -61,12 +63,12 @@ fn get_file_on(file: &str, datetime: &str) -> PyResult<String> {
     let result = get_file_version_on(fileh, dt)
     .map_err(|e| exceptions::ValueError::py_err(format!("get_file_version_on error for {:?} and {:?}: {}",file, dt, e)))?;
 
-    Ok(format!("{}/{}",directory, result))
+    Ok(format!("{}/{}/{}", directory, result, filename.as_str()))
 }
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-fn pybakbuster(py: Python, m: &PyModule) -> PyResult<()> {
+fn pybakbuster(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_function!(get_file_on))?;
     Ok(())
 }
